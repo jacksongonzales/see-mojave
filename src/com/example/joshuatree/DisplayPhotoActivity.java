a5566757245b5176b6ff6e0aa6cefda0508ece79
Jackson Flint-Gonzales <jackson.gonzales@gmail.com>
2013-05-14 13:59:12 -0700

    updates

--- src/com/example/joshuatree/DisplayPhotoActivity.java
+++ src/com/example/joshuatree/DisplayPhotoActivity.java
@@ -3,10 +3,12 @@ package com.example.joshuatree;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class DisplayPhotoActivity extends Activity {
@@ -14,6 +16,11 @@ public class DisplayPhotoActivity extends Activity {
 	private static final String TAG = "JoshuaTree";
 	
 	private ImageView mImageView;
+	private EditText mTitleText;
+	private EditText mBodyText;
+	
+    private Long mRowId;
+    private PhotosDb mDbHelper;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +29,42 @@ public class DisplayPhotoActivity extends Activity {
 		
 		Button addButton = (Button)findViewById(R.id.addButton);
 		
-		Intent displayIntent = getIntent();
-		Bitmap bitmap = (Bitmap) displayIntent.getParcelableExtra("BitmapImage");
+		Intent intent = getIntent();
 		
-		mImageView = (ImageView)findViewById(R.id.imageview);
-		mImageView.setImageBitmap(bitmap); 
+		mTitleText = (EditText) findViewById(R.id.title);
+        mBodyText = (EditText) findViewById(R.id.body);
 		
+	    mImageView = (ImageView)findViewById(R.id.imageview);
+	    
+		// Get the dimensions of the View
+	    int targetW = mImageView.getWidth();
+	    int targetH = mImageView.getHeight();
+		  
+		// Get the dimensions of the bitmap
+	    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+	    bmOptions.inJustDecodeBounds = true;
+	    BitmapFactory.decodeFile(MainActivity.mCurrentPhotoPath, bmOptions);
+	    int photoW = bmOptions.outWidth;
+	    int photoH = bmOptions.outHeight;
+		  
+	    // Determine how much to scale down the image
+	    int scaleFactor = 1;
+		if ((targetW > 0) || (targetH > 0)) {
+			scaleFactor = Math.min(photoW/targetW, photoH/targetH);	
+		}
+		
+	    // Decode the image file into a Bitmap sized to fill the View
+	    bmOptions.inJustDecodeBounds = false;
+	    bmOptions.inSampleSize = scaleFactor;
+	    bmOptions.inPurgeable = true;
+		  
+	    Bitmap bitmap = BitmapFactory.decodeFile(MainActivity.mCurrentPhotoPath, bmOptions);
+	    
+	    mImageView.setImageBitmap(bitmap);
+	
+		
+//		Bitmap bitmap = BitmapFactory.decodeFile(MainActivity.mCurrentPhotoPath);
+				
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             // Show the Up button in the action bar.
             getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -38,6 +75,18 @@ public class DisplayPhotoActivity extends Activity {
 			@Override
 			public void onClick(View view) {
 				// data would be passed to cloud here?
+//				byte[] photo = ImageView; 		// need a function to process image to byte[]
+				String title = mTitleText.getText().toString(); 
+				String body = mBodyText.getText().toString(); 
+				
+				if (mRowId == null) {
+		            long id = mDbHelper.createNote(title, body); // add photo
+		            if (id > 0) {
+		                mRowId = id;
+		            }
+		        } else {
+		            mDbHelper.updateNote(mRowId, title, body); // add photo
+		        }
 				finish();
 			}
 		});
--- src/com/example/joshuatree/MainActivity.java
+++ src/com/example/joshuatree/MainActivity.java
@@ -6,35 +6,50 @@ import java.util.Date;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpledb.AmazonSimpleDBClient;
+import com.amazonaws.services.simpledb.model.CreateDomainRequest;
 
 public class MainActivity extends Activity {
 
 //	private static final String TAG = "JoshuaTree";
 	
 	private Uri fileUri;
-	private Uri mImageUri;
 	
 	public static final int MEDIA_TYPE_IMAGE = 1;
 	private static final int CAMERA_REQUEST_CODE = 1337;
 
-	private Bitmap mImageBitmap;
+	static String mCurrentPhotoPath;
+	
+//	private static LocationManager mLocationManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		LocationManager mLocationManager =
+		        (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		
+		LocationProvider provider =
+		        mLocationManager.getProvider(LocationManager.GPS_PROVIDER);
 	}
 
 	@Override
@@ -44,23 +59,74 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
+	@Override
+	protected void onStart() {
+	    super.onStart();
+
+	    // This verification should be done during onStart() because the system calls
+	    // this method when the user returns to the activity, which ensures the desired
+	    // location provider is enabled each time the activity resumes from the stopped state.
+	    LocationManager locationManager =
+	            (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	    final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+	    if (!gpsEnabled) {
+	        // Do a Toast here asking to enable location grabbing
+	    	// Maybe this
+	    	// if hitOkay {
+	    	enableLocationSettings();
+	    	// }
+	    }
+	}
+
+	private void enableLocationSettings() {
+	    Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+	    startActivity(settingsIntent);
+	}
+	
+	LocationListener locationListener = new LocationListener() {
+	    public void onLocationChanged(Location location) {
+	      // Called when a new location is found by the network location provider.
+//	      makeUseOfNewLocation(location);
+	    }
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+	    
+	
 	public void takePhoto(View view) {
 		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		
 		fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
-//		takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+		takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+		
+//		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 		
 		startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
 	}
 	
-	/** Create a file Uri for saving an image or video */
 	private static Uri getOutputMediaFileUri(int type){
 	      return Uri.fromFile(getOutputMediaFile(type));
 	}
-
-	/** Create a File for saving an image or video */
+	
 	@SuppressLint("SimpleDateFormat")
-	private static File getOutputMediaFile(int type){
+	private static File getOutputMediaFile(int type) {
 	    // To be safe, you should check that the SDCard is mounted
 	    // using Environment.getExternalStorageState() before doing this.
 
@@ -80,21 +146,31 @@ public class MainActivity extends Activity {
 	    // Create a media file name
 	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
 	    File mediaFile;
-	    if (type == MEDIA_TYPE_IMAGE){
+	    if (type == MEDIA_TYPE_IMAGE) {
 	        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
 	        "IMG_"+ timeStamp + ".jpg");
 	    } else {
 	        return null;
 	    }
-
+	    mCurrentPhotoPath = mediaFile.getAbsolutePath();
 	    return mediaFile;
+	}
+
+	private void galleryAddPic() {
+	    Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+	    File f = new File(mCurrentPhotoPath);
+	    Uri contentUri = Uri.fromFile(f);
+	    mediaScanIntent.setData(contentUri);
+	    this.sendBroadcast(mediaScanIntent);
 	}
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == CAMERA_REQUEST_CODE) {
 			if (resultCode == Activity.RESULT_OK) {
+				galleryAddPic();
 				handleSmallCameraPhoto(data);
+//				mLocationManager.removeUpdates(locationListener); 
 			} else if (resultCode == RESULT_CANCELED) {
 				Toast.makeText(this, "Capture Cancelled", Toast.LENGTH_LONG)
 	               .show();
@@ -106,11 +182,13 @@ public class MainActivity extends Activity {
 	}
 	
 	private void handleSmallCameraPhoto(Intent intent) {
-		Bundle extras = intent.getExtras();
-		mImageBitmap = (Bitmap) extras.get("data");
+//		Bundle extras = intent.getExtras();
+//		mImageBitmap = (Bitmap) extras.get("data");
+//		mCurrentPhotoPath.toString(); 
 		Intent displayIntent = new Intent(this, DisplayPhotoActivity.class);
-		displayIntent.putExtra("BitmapImage", mImageBitmap);
+//		displayIntent.putExtra("File Path", mCurrentPhotoPath);
 		startActivity(displayIntent);
         }
 }
+	
 
